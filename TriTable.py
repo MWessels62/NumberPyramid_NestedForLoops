8cdab2878e2b2f7dc96a969df1e1b61d3fb9cf5f
MWessels62 <m.wessels62@gmail.com>
2019-10-01 17:29:02 +0200

    Cleaned up code slightly

--- TriTable.py
+++ TriTable.py
@@ -1,9 +1,6 @@
 #Task 15
 #Compulsory task
-# Write a program that uses nested for loops to create a number pyramid.
-
-# ================= Compulsory Task =================
-# Create a new Python file in this folder called “TriTable.py.” 
+# Write a program that uses nested For-loops to create a number pyramid.
 
 for i in range(1,10):
     for j in range(1,i+1): #each row has entries equivalent to the starting number for that row, therefore we refer to i+1
